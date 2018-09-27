05a59fa75a2c52c3158e5712f238a8521892dd42
SlowL0ris <yuval.friedman@tutanota.com>
2018-09-27 16:24:00 +0300

    moved content ti the startCompetition function

--- src/main/java/org/team2679/TigerEye/core/TigerEye.java
+++ src/main/java/org/team2679/TigerEye/core/TigerEye.java
@@ -1,12 +1,9 @@
 package org.team2679.TigerEye.core;
 
 import edu.wpi.first.wpilibj.RobotBase;
-import edu.wpi.first.wpilibj.SampleRobot;
+import edu.wpi.first.wpilibj.hal.HAL;
 import org.team2679.TigerEye.core.loader.RobotLoader;
-import org.team2679.TigerEye.core.loops.Notifier;
-import org.team2679.TigerEye.core.loops.NotifierListener;
 import org.team2679.TigerEye.lib.log.Logger;
-import org.team2679.TigerEye.lib.util.ConsoleColors;
 
 import java.io.File;
 
@@ -16,42 +13,21 @@ import java.io.File;
  *
  * @author SlowL0ris
  */
-public class TigerEye extends RobotBase  {
+public class TigerEye extends RobotBase {
 
     public static File homeDirectory = new File("/home/slowl0ris/Desktop/Tiger Eye/");
 
     @Override
     public void startCompetition() {
-
-    }
-
-    public static void main(String args[]){
         Logger logger = new Logger("Tiger Eye");
 
         SysOutFileWriter.init();
         RobotLoader.loadModules();
-
-        logger.fatal("crashing!!!!!!!");
-        logger.debug(ConsoleColors.colorize("hello kids, debug message here", ConsoleColors.COLOR.BLUE_BOLD_BRIGHT));
-
-        Notifier notifier = new Notifier("test", 100);
-        notifier.registerListener(new NotifierListener() {
-            @Override
-            public void onUpdate() {
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-        notifier.start();
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        notifier.stop();
     }
 
+    public static void main(String args[]){
+        HAL.initialize(500, 0);
+        TigerEye tigerEye = new TigerEye();
+        tigerEye.startCompetition();
+    }
 }
